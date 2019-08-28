57ff46e1e6769aa265b0b17ff16b8bcf963f3582
Kevin Hoang <hoangkt1@uci.edu>
2019-08-28 00:37:09 -0700

    Changed to $.getJSON and minimized code

--- instagram.js
+++ instagram.js
@@ -1,89 +1,74 @@
 const QUERY_HASH = '472f257a40c653c64c666ce877d59d2b';
 
-var totalPosts;
-var pageID;
-
-var mostComments;
-var mostLikes;
-var mostCommentedPictureURL;
-var mostLikedPictureURL;
+var endCursor, pictures, profileID, totalPosts;
+pictures = [];
 
 function getInstagramHandle() {
     return document.getElementById('instagram-handle').value;
 }
 
 function getInstagramURL(instagramHandle) {
-    return `https://www.instagram.com/${instagramHandle}?_a=1`;
-}
-
-function getTotalPosts(instagramURL) {
-    $.get(instagramURL, function(markup) {
-        var doc = new DOMParser().parseFromString(markup, 'text/html');
-        console.log(doc);
-        var meta = doc.querySelector('meta[property="og:description"]');
-        var value = meta && meta.getAttribute('content');
-        
-        totalPosts = parseInt(value.split(" ")[4], 10);
-    });
-}
-
-function getPageID(instagramURL) {
-    $.get(instagramURL, function(markup) {
-        var doc = new DOMParser().parseFromString(markup, 'text/html');
-        var meta = doc.querySelectorAll('script[type="text/javascript"]')[3];
-        var value = meta.innerHTML;
-
-        pageID = value.substring(value.indexOf('profilePage_') + 12, value.indexOf('show_suggested_profiles') - 3);
-    });
+    return `https://www.instagram.com/${instagramHandle}/?__a=1`;
 }
 
 function getInstagramGraphURL() {
-    console.log(`https://www.instagram.com/graphql/query/?query_hash=${QUERY_HASH}&variables={"id":"${pageID}","first":${totalPosts}}`)
-    return `https://www.instagram.com/graphql/query/?query_hash=${QUERY_HASH}&variables={"id":"${pageID}","first":${totalPosts}}`;
+    return `https://www.instagram.com/graphql/query/?query_hash=${QUERY_HASH}&variables={"id":"${profileID}","first":${totalPosts},"after":"${endCursor}"}`;
 }
 
-function getPictures(instagramGraphURL) {
-    mostComments = 0;
-    mostLikes = 0;
+function getGraphQLVariables(instagramURL) {
+    $.getJSON({
+        url: instagramURL,
+        success: function(data) {
+            endCursor = data.graphql.user.edge_owner_to_timeline_media.page_info.end_cursor;
+            profileID = data.logging_page_id.substring(12);
+            totalPosts = data.graphql.user.edge_owner_to_timeline_media.count;
+        },
+        error: function(data) {
+            console.log('FAILED');
 
-    $.get(instagramGraphURL, function(markup) {
-        for (i = 0; i < 24; i++) {
-            var node = markup['data']['user']['edge_owner_to_timeline_media']['edges'][`${i}`]['node'];
-            var displayURL = node['display_url']
-
-            if (mostLikes < node['edge_media_preview_like']['count']) {
-                mostLikes = node['edge_media_preview_like']['count'];
-                mostLikedPictureURL = displayURL;
-            }
-
-            if (mostComments < node['edge_media_to_comment']['count']) {
-                mostComments = node['edge_media_to_comment']['count'];
-                mostCommentedPictureURL = displayURL;
-            }
+            endCursor = 'QVFDMWhoQ3dSQWpTOVo1QXhPRmc2YWw0NF8wRy1Na0tiRjVuZjZYZFFIbC1yamUwX05xelhERk9samY1Xzk3cUhZb0ZNWkZTbHV3TFFHM1ROMjNIRWVaMQ==';
+            profileID = '438970162';
+            totalPosts = '120'
         }
-    });
+    })
 }
 
-function displayPictures() {
-    document.getElementById('most-liked-picture').src = mostLikedPictureURL;
-    document.getElementById('most-commented-picture').src = mostCommentedPictureURL;
+function getTopPictures(instagramGraphURL) {
+    console.log(instagramGraphURL);
+
+    $.getJSON({
+        url: instagramGraphURL,
+        success: function(data) {
+            var edges = data.data.user.edge_owner_to_timeline_media.edges;
+            endCursor = data.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
+            
+            // Recursive call to load more photos
+            if (endCursor !== null) {
+                getTopPictures(getInstagramGraphURL());
+            }
+
+            for (i = 0; i < edges.length; i++) {
+                pictures.push([edges[i].node.edge_media_preview_like.count, edges[i].node.thumbnail_src]);
+            }
+
+            pictures.sort((picX, picY) => {return picY[0] - picX[0]});
+        }
+    })
 }
 
 document.getElementById('instagram-handle').addEventListener('keyup', function(e) {
-    if (e.which === 13 || e.keyCode === 13) {
-        var instagramURL = getInstagramURL(getInstagramHandle());
+    if (e.keyCode === 13) {
 
-        getTotalPosts(instagramURL);
-        getPageID(instagramURL);
-        getPictures(getInstagramGraphURL());
+        getGraphQLVariables(getInstagramURL(getInstagramHandle()));
+        getTopPictures(getInstagramGraphURL());
+        console.log(pictures);
+        console.log(pictures[0][1]);
         displayPictures();
     }
 });
 
-document.getElementById('submit-button').addEventListener('click', () => {
-    var instagramURL = getInstagramURL(getInstagramHandle());
-    getTotalPosts(instagramURL);
-    getPageID(instagramURL);
-    getPictures(getInstagramGraphURL());
-    displayPictures();
-});
+function displayPictures() {
+    for (i = 1; i <= 5; i++) {
+        document.getElementById(`picture${i}`).src = pictures[i][1];
+    }
+}
